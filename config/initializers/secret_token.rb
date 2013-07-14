0af14946a32edf9d0484fb9e63342604bf3bda8c
Marcos Gaeta <mgaeta89@gmail.com>
2013-07-14 14:37:38 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WhatDoesMarcosEat::Application.config.secret_token = '3c4a312bd06873f3d22ba94e97553c76af33b55dc048b3d9d87872c2845738646a3297810eb6e8b80213e0ff23b0b4db9d7b3e7a946ae3c0cef63f75b13f5292'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+WhatDoesMarcosEat::Application.config.session_store :cookie_store, key: '_what-does-marcos-eat_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# WhatDoesMarcosEat::Application.config.session_store :active_record_store
